3242afa6b8fc3ae9fbe8b6cd85bbc12e7315f433
howie1329 <46204910+howie1329@users.noreply.github.com>
2026-01-02 00:24:19 -0500

    Update package dependencies and global styles; integrate new UI components and enhance layout with improved font handling and color variables.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "ListIt",
@@ -28,9 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <ConvexAuthNextjsServerProvider>
-      <html lang="en">
+      <html lang="en" className={inter.variable}>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased w-svw h-svh overflow-hidden`}
+          className={`${inter.variable} antialiased w-svw h-svh overflow-hidden`}
         >
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
