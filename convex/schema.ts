fe94c76c29053d2a9272bcba64ebecff49bdf93d
howie1329 <46204910+howie1329@users.noreply.github.com>
2026-04-24 22:00:39 -0400

    feat:install tanstack-start and convex and convex auth

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+import { defineSchema, defineTable } from 'convex/server'
+import { authTables } from "@convex-dev/auth/server";
+import { v } from 'convex/values'
+
+export default defineSchema({
+  ...authTables,
+  products: defineTable({
+    title: v.string(),
+    imageId: v.string(),
+    price: v.number(),
+  }),
+  todos: defineTable({
+    text: v.string(),
+    completed: v.boolean(),
+  }),
+})
